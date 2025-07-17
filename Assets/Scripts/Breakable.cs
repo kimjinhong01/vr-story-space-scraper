ef58b9c2220e92ca102b0f94d302fbb3e021e4cb
Michael <kimjinhong01@naver.com>
2025-07-17 21:05:22 +0900

    feat: 프로젝트 파일 추가

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Breakable : MonoBehaviour
+{
+    public List<GameObject> breakablePieces;
+    public float timeToBreak = 2;
+    private float timer = 0;
+    public UnityEvent OnBreak;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var item in breakablePieces)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    public void Break()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > timeToBreak)
+        {
+            gameObject.GetComponent<MeshCollider>().enabled = false;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+            foreach (var item in breakablePieces)
+            {
+                item.SetActive(true);
+                item.transform.parent = null;
+            }
+
+            OnBreak.Invoke();
+
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlaySteps.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlaySteps : MonoBehaviour
+{
+    PlayableDirector director;
+    public List<Step> steps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    [Serializable]
+    public class Step
+    {
+        public string name;
+        public float time;
+        public bool hasPlayed = false;
+    }
+
+    public void PlayStepIndexc(int index)
+    {
+        Step step = steps[index];
+
+        if (!step.hasPlayed)
+        {
+            step.hasPlayed = true;
+
+            director.Stop();
+            director.time = step.time;
+            director.Play();
+        }
+    }
+}
